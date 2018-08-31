0cf6724c2eb556a86e19840f211ce61e80de899d
alpcanaydin <alpcan@alpcanaydin.com>
2018-08-31 22:33:06 +0300

    Components indexed updated for new components.

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 // @flow
 
-// eslint-disable-next-line import/prefer-default-export
+export { default as Container } from './Container';
+export { default as Header } from './Header';
+export { default as Hero } from './Hero';
 export { default as Logo } from './Logo';
