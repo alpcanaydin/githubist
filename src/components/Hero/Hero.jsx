0c55278c189d04b826102d478ee6defeb470f2fd
alpcanaydin <alpcan@alpcanaydin.com>
2018-09-03 01:18:50 +0300

    Hero component node tree updated.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,23 +13,21 @@ type Props = {
 };
 
 const Hero = ({ description }: Props) => (
-  <div className={styles.hero}>
-    <Container>
-      <div className={styles.inner}>
-        <div>
-          <h6 className={styles.badge}>2018</h6>
-          <h1 className={styles.title}>
-            Github
-            <br />
-            Türkiye İstatistikleri
-          </h1>
-          <p className={styles.description}>{description}</p>
-        </div>
-
-        <img src={githubLogo} className={styles.image} alt="Github" width={431} height={420} />
+  <Container>
+    <div className={styles.hero}>
+      <div>
+        <h6 className={styles.badge}>2018</h6>
+        <h1 className={styles.title}>
+          Github
+          <br />
+          Türkiye İstatistikleri
+        </h1>
+        <p className={styles.description}>{description}</p>
       </div>
-    </Container>
-  </div>
+
+      <img src={githubLogo} className={styles.image} alt="Github" width={431} height={420} />
+    </div>
+  </Container>
 );
 
 export default Hero;
