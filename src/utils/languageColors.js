213fc2ae584c32bba2ef8e580924bd54a0ce1a9f
alpcanaydin <alpcan@alpcanaydin.com>
2018-09-03 01:19:13 +0300

    Language colors util added.

---
+++ src/utils/languageColors.js
@@ -0,0 +1,35 @@
+// @flow
+
+// Colors are hard-coded since this is a static data.
+// It is a little dirty solution and this file needs to be updated when the language rank changed.
+
+type Color = {
+  backgroundColor: string,
+  color: string,
+};
+
+const colors = {
+  javascript: {
+    backgroundColor: '#ffc157',
+    color: '#2d2e33',
+  },
+
+  java: {
+    backgroundColor: '#5781ff',
+    color: '#fff',
+  },
+
+  python: {
+    backgroundColor: '#FFDD57',
+    color: '#2d2e33',
+  },
+
+  go: {
+    backgroundColor: '#57c9ff',
+    color: '#2d2e33',
+  },
+};
+
+const languageColors = (slug: string): Color => colors[slug];
+
+export default languageColors;
