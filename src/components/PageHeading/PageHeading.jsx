696c26b6525e25002222062c351150855f3b2575
alpcanaydin <alpcan@alpcanaydin.com>
2018-09-07 21:06:21 +0300

    Components updated.

--- src/components/PageHeading/PageHeading.jsx
+++ src/components/PageHeading/PageHeading.jsx
@@ -2,8 +2,6 @@
 
 import React from 'react';
 
-import { Container } from '..';
-
 import styles from './PageHeading.scss';
 
 type Props = {
@@ -13,10 +11,8 @@ type Props = {
 
 const PageHeading = ({ title, subtitle }: Props) => (
   <section className={styles.container}>
-    <Container>
-      <h1 className={styles.title}>{title}</h1>
-      {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
-    </Container>
+    <h1 className={styles.title}>{title}</h1>
+    {subtitle && <p className={styles.subtitle}>{subtitle}</p>}
   </section>
 );
 
