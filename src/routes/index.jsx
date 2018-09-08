97280cdec35d62f14bacb674fdc0b9821cc29d04
alpcanaydin <alpcan@alpcanaydin.com>
2018-09-08 13:57:58 +0300

    Repositories added to router.

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,10 +4,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Homepage from './Homepage';
+import Repositories from './Repositories';
 
 const Routes = () => (
   <Switch>
     <Route exact path="/" component={Homepage} />
+    <Route path="/repositories" component={Repositories} />
   </Switch>
 );
 
